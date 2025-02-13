49e747da992d48d639ba2486fb594a082af2aba6
adityansh777 <adityansh.kapasia@peoplestrong.com>
2025-02-13 13:07:03 +0530

    Login comp added and index html fixed for oversize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@
 import { Component } from '@angular/core';
 import { NgIf } from '@angular/common'; // Import NgIf
 import { WelcomeComponent } from './components/welcome/welcome.component'; // Import the WelcomeComponent
+import { LoginComponent } from './components/login/login.component'; // Import the LoginComponent
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [NgIf, WelcomeComponent],
+  imports: [NgIf, WelcomeComponent, LoginComponent],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  username = 'adityansh';
+  password = 'password';
+
 }
