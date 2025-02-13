7f2b1d2fffed10d66bef60a0a081c27572ff7c47
adityansh777 <adityansh.kapasia@peoplestrong.com>
2025-02-13 12:13:55 +0530

    welcome component test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 // app.component.ts
 import { Component } from '@angular/core';
 import { NgIf } from '@angular/common'; // Import NgIf
-
-
+import { WelcomeComponent } from './components/welcome/welcome.component'; // Import the WelcomeComponent
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [NgIf],
+  imports: [NgIf, WelcomeComponent],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
