fee44ed25c3e1d51849c1e1d5aca0a3ca8d7accb
adityansh777 <adityansh.kapasia@peoplestrong.com>
2025-02-14 14:45:42 +0530

    ngif for wheather logged in or not

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { HardcodedauthenticationService } from '../../service/hardcodedauthentication.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
 })
-export class MenuComponent {}
+export class MenuComponent implements OnInit {
+  isUserLoggedIn: Boolean = false;
+  constructor(
+    public hardcodedauthenticationservice: HardcodedauthenticationService
+  ) {}
+  ngOnInit() {
+    this.isUserLoggedIn = this.hardcodedauthenticationservice.isuserloggedin();
+  }
+}
