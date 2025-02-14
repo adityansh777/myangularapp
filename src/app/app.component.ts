8bb2205d7492876b7fca758928dcbadfa9076c1a
adityansh777 <adityansh.kapasia@peoplestrong.com>
2025-02-14 12:53:06 +0530

    bootstrap nav and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 // app.component.ts
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router'; // Import RouterModule
-import { WelcomeComponent } from './components/welcome/welcome.component';
-import { MainwelcomeComponent } from './components/mainwelcome/mainwelcome.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [RouterModule, WelcomeComponent, MainwelcomeComponent],
+  imports: [RouterModule, FooterComponent, MenuComponent],
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
