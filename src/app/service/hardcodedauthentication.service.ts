d8ddfa958d12e18f502a8e7eb8c07a31e0c177ec
adityansh777 <adityansh.kapasia@peoplestrong.com>
2025-02-14 14:14:05 +0530

    extra welcome removed

--- src/app/service/hardcodedauthentication.service.ts
+++ src/app/service/hardcodedauthentication.service.ts
@@ -8,9 +8,15 @@ export class HardcodedauthenticationService {
 
   authenticate(username: string, password: string) {
     if (username === 'adityansh' && password === 'password') {
+      sessionStorage.setItem('authuser', username);
       return true;
     } else {
       return false;
     }
+  }
+
+  isuserloggedin() {
+    let user = sessionStorage.getItem('authuser');
+    return !(user === null);
   }
 }
