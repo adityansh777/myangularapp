9b53666294a0053b60ab58a926eda9c3be616c7f
adityansh777 <adityansh.kapasia@peoplestrong.com>
2025-02-20 16:07:30 +0530

    welcome route update to use headers and interceptor files created

--- src/app/services/data/welcomedata/welcome-data.service.ts
+++ src/app/services/data/welcomedata/welcome-data.service.ts
@@ -1,5 +1,5 @@
 // src/app/services/welcome-data.service.ts
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -21,8 +21,21 @@ export class WelcomeDataService {
   }
 
   executeHelloWorldServiceWithPathVariable(name: string): Observable<any> {
+    let basicAuthHeaderString = this.createBasicAuthenticationHttpHeader();
+
+    let headers = new HttpHeaders({
+      Authorization: basicAuthHeaderString,
+    });
     return this.http.get<HelloWorldBean>(
-      `http://localhost:8080/hello-world/path-variable/${name}`
+      `http://localhost:8080/hello-world/path-variable/${name}`,
+      { headers }
     );
+  }
+  createBasicAuthenticationHttpHeader() {
+    let username = 'in28minutes';
+    let password = 'dummy';
+    let basicAuthHeaderString =
+      'Basic ' + window.btoa(username + ':' + password);
+    return basicAuthHeaderString;
   }
 }
--- src/app/services/http/http-interceptor-basic-auth.service.ts
+++ src/app/services/http/http-interceptor-basic-auth.service.ts
@@ -1,32 +1,9 @@
-import {
-  HttpEvent,
-  HttpHandler,
-  HttpInterceptor,
-  HttpRequest,
-} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class HttpInterceptorBasicAuthService implements HttpInterceptor {
-  constructor() {}
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    console.log('HTTP Interceptor invoked');
-    let username = 'in28minutes';
-    let password = 'dummy';
-    let basicAuthHeaderString =
-      'Basic ' + window.btoa(username + ':' + password);
+export class HttpInterceptorBasicAuthService {
 
-    req = req.clone({
-      setHeaders: {
-        Authorization: basicAuthHeaderString,
-      },
-    });
-    return next.handle(req);
-  }
+  constructor() { }
 }
