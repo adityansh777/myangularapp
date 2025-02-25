8e4257a13bc9f8ac4c0068fb12cff6c52b697098
adityansh777 <adityansh.kapasia@peoplestrong.com>
2025-02-25 12:29:51 +0530

    redesign 2

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -28,17 +28,15 @@ export class LoginComponent {
     this.jwtAuthService.authenticate(this.username, this.password).subscribe(
       (data) => {
         console.log('Login successful! with username', this.username);
-        // Set flags for a successful login
+
         this.invalidLogin = false;
         this.validLogin = true;
-        // Navigate to welcome page using window.location.href (full reload)
+        // full reload
         window.location.href = '/welcome/' + this.username;
-        // Alternatively, if you prefer Angular navigation, you could use:
         // this.router.navigate(['welcome', this.username]);
       },
       (error) => {
         console.log(error);
-        // Set flags for an unsuccessful login
         this.invalidLogin = true;
         this.validLogin = false;
       }
